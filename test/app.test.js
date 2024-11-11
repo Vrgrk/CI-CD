77e0a3d43a73f208b986c63c4b7d5c42c26f265f
Vrgrk <vimalrg.radhakrishnan@thebluespire.com>
2024-11-11 12:35:42 +0530

    first_commit

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+describe('sample-test-cases',()=>{
+    test("First test cases",()=>{
+        expect(1+1).toBe(2)
+
+    })
+
+    test("Second test cases",()=>{
+        expect(1+2).toBe(3)
+    })
+})
