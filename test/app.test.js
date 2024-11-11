ecf9bbb9e5bf9489d6b6ab91960ffd58a5434468
Vrgrk <vimalrg.radhakrishnan@thebluespire.com>
2024-11-11 13:06:19 +0530

    third commit

--- test/app.test.js
+++ test/app.test.js
@@ -7,4 +7,8 @@ describe('sample-test-cases',()=>{
     test("Second test cases",()=>{
         expect(1+2).toBe(3)
     })
+
+    test("thirs test cases",()=>{
+        expect(1+4).toBe(5)
+    })
 })
